dd18ff4dbf207c0b38992f13fa72dcc8aaf8ab58
kipkukelvin <kipkukelvin1@gmail.com>
2025-12-11 16:37:49 +0300

    Update layouts.blade.php

--- resources/views/partials/layouts.blade.php
+++ resources/views/partials/layouts.blade.php
@@ -7,7 +7,7 @@
     <meta name="generator" content="Astro v5.13.2" />
 
     <title>@yield('title', 'Page Title')</title>
-    <link rel="icon" type="image/png/jpg" href="{{ asset('assets/images/log1.jpg') }}?v=1">
+    <link rel="icon" type="image/png/jpg" href="{{ asset('assets/img/LOGO.jpg') }}?v=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <meta name="theme-color" content="#712cf9" />
